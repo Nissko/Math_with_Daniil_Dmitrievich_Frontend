6d8959dc1f44418f81fa9063f5793665b8442f33
Nikita Skibko <skibko.nv@mmk.ru>
2025-06-28 12:54:36 +0500

    Доработка функционала выбора предмета

---
+++ MathProject.Client/Enums/SubjectEnumeration/ClientSubjectEnumeration.cs
@@ -0,0 +1,22 @@
+﻿namespace MathProject.Client.Enums.SubjectEnumeration;
+
+/// <summary>
+/// Класс для работы выбора предметов
+/// ВАЖНО: Нужно заполнить после деплоя на хостинг (указать актульные Guid предметов)
+/// При добавлении новых предметов, тоже нужно добавить сюда
+/// </summary>
+public static class ClientSubjectEnumeration
+{
+    public static readonly Subject Math = new Subject(id: Guid.Parse("d7787d23-15c3-45c1-ad6c-0904c73451f7"));
+    public static readonly Subject Inform = new Subject(id: Guid.Parse("e2232288-14d2-47f7-8919-c3f46eacc6ac"));
+    
+    public class Subject
+    {
+        public Guid Id { get; }
+
+        internal Subject(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
