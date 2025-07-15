023eb47690847c6df81dc0fb4b262315eb8f2f3c
Nikita Skibko <skibko.nv@mmk.ru>
2025-07-15 15:43:51 +0500

    Доделал весь функционал верхней схемы(Итог)

--- MathProject.Client/DTOS/SubthemesOfLearning/ClientSubthemesOfLearningTaskSelectDTO.cs
+++ MathProject.Client/DTOS/SubthemesOfLearning/ClientSubthemesOfLearningTaskSelectDTO.cs
@@ -2,5 +2,9 @@
 
 public record ClientSubthemesOfLearningTaskSelectDTO
 {
-    
+    public Guid Id { get; init; }
+    public Guid LearningTopicId { get; init; }
+    public string Name { get; init; }
+    public int DisplayOrder { get; init; }
+    public bool IsVisible { get; init; }
 }
--- MathProject.Client/Program.cs
+++ MathProject.Client/Program.cs
@@ -1,11 +1,24 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MathProject.Client;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
